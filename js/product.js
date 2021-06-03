cf9e9ff9c091130acb2fb4a6a819380ca2162faf
JoakimWibe <71314249+JoakimWibe@users.noreply.github.com>
2021-06-03 15:09:45 +0200

    improved the product page

--- js/product.js
+++ js/product.js
@@ -29,8 +29,12 @@ function createHTML(details) {
 
     document.title = details.name;
 
-    detailContainer.innerHTML = `<h1 class="details-name">${details.name}</h1>
+    detailContainer.innerHTML = `
                                 <div class="details-image" style="background-image: url(${details.images[0].src})"></div>
-                                <p class="description">${details.description}</p>
-                                <h2 class="price">Price: ${details.prices.price}$</h2>`;                    
+                                <div class="info">
+                                    <h1 class="details-name">${details.name}</h1>
+                                    <p class="description">${details.description}</p>
+                                    <h2 class="price">Price: ${details.prices.price}$</h2>
+                                    <div class="add-cart">Add to cart</div>
+                                </div>`;                    
 }
